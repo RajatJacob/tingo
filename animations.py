1cbb1a56c48c28f5718d75521b45e54e8474abdd
Rajat Jacob <rajatjacob@gmail.com>
2017-03-28 11:01:11 +0530

    Gamma color correction

--- animations.py
+++ animations.py
@@ -6,11 +6,29 @@ LED_COUNT   = 48      # Number of LED pixels.
 LED_PIN     = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA     = 5       # DMA channel to use for generating signal (try 5)
-#LED_BRIGHTNESS = 255  # LED Brightness
+LED_BRIGHTNESS = 255  # LED Brightness
 LED_INVERT  = False   # True to invert the signal (when using NPN transistor level shift)
 
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT)
 strip.begin()
+
+gamma = [
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255] 
 
 def colorWipe(arg):
 	"""Wipe color across display a pixel at a time."""
@@ -18,9 +36,9 @@ def colorWipe(arg):
 	if(len(arg) < 2):
 		arg.append(50)
 	color = hexToArray(arg[0])
-	color[0] = (color[0])/3
-	color[1] = (color[1])/2
-	color[2] = (color[2])/1
+	color[0] = gamma[color[0]]
+	color[1] = gamma[color[1]]
+	color[2] = gamma[color[2]]
 	color = ArrayToHex(color)
 	color = hexToColor(color)
 	for i in range(strip.numPixels()):
