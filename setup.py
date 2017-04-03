b8733f8ec3f4d9714056c009aade89b15224878c
Rajat Jacob <rajatjacob@gmail.com>
2017-04-03 16:26:32 +0530

    Fixed setup

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ s = s + raw_input("Password: ")
 s = s + "\',\n)\n\nclient = dict(\n\tdev_id = \'"
 print "\nClient Configuration\n"
 s = s + raw_input("Device ID: ")
-s = s + "\',\n\ttype = "
+s = s + "\',\n\ttype = \'"
 s = s + raw_input("Device Type: ")
 s = s + "\',\n\tpin = "
 s = s + raw_input("GPIO Pin: ")
-s = s + "\',\n)"
+s = s + ",\n)"
 file = open("conf.py", "w")
 file.write(s)
 file.close()
