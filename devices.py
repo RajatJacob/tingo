69e4a0acb68df5f39685d1e74b7586b000989b15
Rajat Jacob <rajatjacob@gmail.com>
2020-05-23 03:44:54 +0100

    Basics working. TODO: configfile and MQTT

--- devices.py
+++ devices.py
@@ -2,22 +2,24 @@ import RPi.GPIO as GPIO
 
 class Device():
 	def __init__(self, pin, type=GPIO.OUT):
+		print("Created Device at pin #{} of type {}".format(pin, 'in' if type==GPIO.IN else 'out' ))
 		self.pin = pin
 		if type not in [GPIO.IN, GPIO.OUT]:
 			raise Exception("Device type should be RPi.GPIO.IN or RPi.GPIO.OUT")
-			self.type = type
+		self.type = type
+		GPIO.setup(self.pin, self.type)
 
 class DigitalDevice(Device):
-	def __init__(self, pin, type, state=False):
-		super(DigitalDevice, self).__init__(pin, type)
-		GPIO.setup(self.pin, self.type)
-		setState(state)
-
 	def setState(self, state):
 		self.state = state
-		GPIO.output(self.pin, self.state)
+		GPIO.output(self.pin, not self.state)
+		return self.state
 
 	def getState(self):
 		if self.type == GPIO.IN:
 			self.state = GPIO.input(self.pin)
 		return self.state
+
+	def __init__(self, pin, type, state=False):
+		super().__init__(pin, type)
+		self.setState(state)
--- tingo.py
+++ tingo.py
@@ -1,16 +1,15 @@
 import RPi.GPIO as GPIO
 from devices import DigitalDevice
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 lamp = DigitalDevice(16, GPIO.OUT)
 
-print(lamp.getState())
 
-lamp.setState(True)
+states = [True, False, True, False, True]
 
-print(lamp.getState())
-
-lamp.setState(False)
-
-print(lamp.getState())
+import time
+for s in states:
+	print(lamp.setState(s))
+	time.sleep(1)
